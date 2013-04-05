802ea5b01b54ec5da43a8d28d23ccab9e8222f11
Luis Gustavo <gustavoschirmer@hotmail.com>
2013-04-04 21:55:15 -0400

    primeira interface do menu

--- Application.java
+++ Application.java
@@ -2,9 +2,51 @@ class Application {
 	
 	public static void main(String[] asas){
 
+		Application a = new Application();
+
+	}
+
+	public Application (){
+
 		DataHandler dh = new DataHandler("users.txt");
 		dh.list();
 
+	}	
+
+	public void showMenu(){
+
+			System.out.println("Bem Vindos a UsersControl");
+			System.out.println("Por favor selecione uma opcao");
+			System.out.println("1-Adicionar");
+			System.out.println("2-Listar");
+			System.out.println("3-Remover");
+			System.out.println("4-Exportar");
+			System.out.println("5-Importar");
+	}
+
+	public void adicionar(){
+
+			//trampo do felipe
+	}
+
+	public void listar(){
+
+			//trampo do felipe
+	}
+
+	public void remover(){
+
+			//trampo do felipe
+	}
+
+	public void exportar(){
+
+			//trampo do felipe
+	}
+
+	public void importar(){
+
+			//trampo do felipe
 	}
 
 	//add @ User
